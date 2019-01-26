cc97c0874a4a909d986d987c2c0b6e40ac23331c
Dusk Eagle <Dusk.Eagle@gmail.com>
2019-01-26 13:33:00 -0800

    Sorting out edge cases in MTGA log

--- mtgatracker.go
+++ mtgatracker.go
@@ -22,9 +22,11 @@ func GetTransactionStrings(mtgaLog string) []string {
 			current_str_arr = append(current_str_arr, line)
 		}
 		if strings.HasPrefix(line, "}") {
-			processing_json = false
-			all_transaction_strings = append(all_transaction_strings, strings.Join(current_str_arr, "\n"))
-			current_str_arr = []string{}
+			if processing_json {
+				processing_json = false
+				all_transaction_strings = append(all_transaction_strings, strings.Join(current_str_arr, "\n"))
+				current_str_arr = []string{}
+			}
 		}
 	}
 	return all_transaction_strings
@@ -37,42 +39,46 @@ func GetTransactions(mtgaLog string) []Transaction {
 		var t Transaction
 		err := json.Unmarshal([]byte(transaction_string), &t)
 		if err != nil {
-			log.Print(fmt.Sprintf("Transaction number: %d", line))
-			log.Print(fmt.Sprintf("Transaction contents: %s", transaction_string))
-			log.Fatal(err)
+			if err.Error() != "json: cannot unmarshal number into Go struct field Transaction.timestamp of type string" {
+				log.Print(fmt.Sprintf("Transaction number: %d", line))
+				log.Print(fmt.Sprintf("Transaction contents: %s", transaction_string))
+				log.Fatal(err)
+			}
 		}
 		transactions = append(transactions, t)
 	}
 	return transactions
 }
 
+func GetTurn(transaction Transaction) (int, bool) {
+	greMessages := transaction.GreToClientEvent.GreToClientMessages
+	for _, greMessage := range greMessages {
+		turnInfo := greMessage.GameStateMessage.TurnInfo
+		if turnInfo != nil {
+			return turnInfo.TurnNumber, true
+		}
+	}
+	return 0, false
+}
+
 /*
  * Returns a map of player id to map of turns to array of card ids.
  */
-func GetHands(transactions []Transaction) map[int]map[int][]int {
-	hand := map[int]map[int][]int{}
-	for _, transaction := range transactions {
-		greMessages := transaction.GreToClientEvent.GreToClientMessages
-		for _, greMessage := range greMessages {
-			turnInfo := greMessage.GameStateMessage.TurnInfo
-			turn := -1
-			if turnInfo != nil {
-				turn = turnInfo.TurnNumber
-			}
-			zones := greMessage.GameStateMessage.Zones
-			if zones != nil {
-				for _, zone := range *zones {
-					if zone.Type == "ZoneType_Hand" {
-						if hand[zone.OwnerSeatId] == nil {
-							hand[zone.OwnerSeatId] = map[int][]int{}
-						}
-						hand[zone.OwnerSeatId][turn] = zone.ObjectInstanceIds
-					}
+func GetHands(transaction Transaction) (map[int][]int, bool) {
+	hand := map[int][]int{}
+	greMessages := transaction.GreToClientEvent.GreToClientMessages
+	for _, greMessage := range greMessages {
+		zones := greMessage.GameStateMessage.Zones
+		if zones != nil {
+			for _, zone := range *zones {
+				if zone.Type == "ZoneType_Hand" {
+					hand[zone.OwnerSeatId] = zone.ObjectInstanceIds
 				}
 			}
+			return hand, true
 		}
 	}
-	return hand
+	return nil, false
 }
 
 func GetCardData() map[int]Card {
@@ -100,79 +106,75 @@ func GetCardData() map[int]Card {
  * Returns a mapping of the game object id of a card to the id of a card in the master
  * MTGA database.
  */
-func GetGameObjectCards(transactions []Transaction) map[int]int {
+func GetGameObjectCards(transaction Transaction) (map[int]int, bool) {
 	cardMap := map[int]int{}
-	for _, transaction := range transactions {
-		greMessages := transaction.GreToClientEvent.GreToClientMessages
-		for _, greMessage := range greMessages {
-			gameObjects := greMessage.GameStateMessage.GameObjects
-			if gameObjects != nil {
-				for _, gameObject := range *gameObjects {
-					if gameObject.Type == "GameObjectType_Card" {
-						cardMap[gameObject.InstanceId] = gameObject.GrpId
-					}
-				} 
-			}
+	greMessages := transaction.GreToClientEvent.GreToClientMessages
+	for _, greMessage := range greMessages {
+		gameObjects := greMessage.GameStateMessage.GameObjects
+		if gameObjects != nil {
+			for _, gameObject := range *gameObjects {
+				if gameObject.Type == "GameObjectType_Card" {
+					cardMap[gameObject.InstanceId] = gameObject.GrpId
+				}
+			} 
 		}
+		
 	}
-	return cardMap
+	if len(cardMap) > 0 {
+		return cardMap, true
+	}
+	return nil, false
 }
 
 /*
  * Get the winner's seat Id
  */
-func GetWinner(transactions []Transaction) int {
-	for _, transaction := range transactions {
-		greMessages := transaction.GreToClientEvent.GreToClientMessages
-		for _, greMessage := range greMessages {
-			gameInfo := greMessage.GameStateMessage.GameInfo
-			if gameInfo != nil {
-				results := gameInfo.Results
-				for _, result := range results {
-					if result.Result == "ResultType_WinLoss" {
-						return result.WinningTeamId
-					}
+func GetWinner(transaction Transaction) (int, bool) {
+	greMessages := transaction.GreToClientEvent.GreToClientMessages
+	for _, greMessage := range greMessages {
+		gameInfo := greMessage.GameStateMessage.GameInfo
+		if gameInfo != nil {
+			results := gameInfo.Results
+			for _, result := range results {
+				if result.Result == "ResultType_WinLoss" {
+					return result.WinningTeamId, true
 				}
 			}
 		}
 	}
-	log.Fatal("No winner found")
-	return 0
+	return 0, false
 }
 
 /*
  * Returns a map of seatId to player names
  */
-func GetPlayerSeats(transactions []Transaction) map[int]string {
+func GetPlayerSeats(transaction Transaction) (map[int]string, bool) {
 	seatMap := map[int]string{}
-	for _, transaction := range transactions {
-		gameRoom := transaction.MatchGameRoomStateChangedEvent
-		if gameRoom != nil {
-			players := gameRoom.GameRoomInfo.GameRoomConfig.ReservedPlayers
-			for _, player := range players {
-				seatMap[player.SystemSeatId] = player.PlayerName
-			} 
+	gameRoom := transaction.MatchGameRoomStateChangedEvent
+	if gameRoom != nil {
+		players := gameRoom.GameRoomInfo.GameRoomConfig.ReservedPlayers
+		for _, player := range players {
+			seatMap[player.SystemSeatId] = player.PlayerName
 		}
+		return seatMap, true
 	}
-	return seatMap
+	return nil, false
 }
 
-func handToCardNames(hands map[int][]int, cardData map[int]Card, gameObjectCards map[int]int) []string {
-	handCardNames := make([]string, 0, len(hands))
-	for _, playerHands := range hands {
-		for _, cardId := range playerHands {
-			gameObjectCardId := gameObjectCards[cardId]
-			card, ok := cardData[gameObjectCardId]
-			if !ok {
-				log.Fatal("Card with game ID " + strconv.Itoa(cardId) + " and MTGA ID " + strconv.Itoa(gameObjectCardId) + " not found in card data.")
-			}
-			handCardNames = append(handCardNames, card.Name)
+func HandToCardNames(hand []int, cardData map[int]Card, gameObjectCards map[int]int) []string {
+	handCardNames := make([]string, 0, len(hand))
+	for _, cardId := range hand {
+		gameObjectCardId := gameObjectCards[cardId]
+		card, ok := cardData[gameObjectCardId]
+		if !ok {
+			log.Fatal("Card with game ID " + strconv.Itoa(cardId) + " and MTGA ID " + strconv.Itoa(gameObjectCardId) + " not found in card data.")
 		}
+		handCardNames = append(handCardNames, card.Name)
 	}
 	return handCardNames
 }
 
-func printWinner(userName string, playerSeats map[int]string, winner int, handCardNames []string) {
+func PrintWinner(userName string, playerSeats map[int]string, winner int, handCardNames []string) {
 	wonOrLost := ""
 	if playerSeats[winner] == userName {
 		wonOrLost = "won"
@@ -182,34 +184,77 @@ func printWinner(userName string, playerSeats map[int]string, winner int, handCa
 	fmt.Println("You " + wonOrLost + " with cards \"" + strings.Join(handCardNames, "\", \"") + "\".")
 }
 
+func GetGames(transactions []Transaction, cardData map[int]Card, userName string) (string, []string) {
+	playerSeats := map[int]string{}
+	userPlayerSeat := 0
+	turn := 0
+	hand := map[int]struct{}{}
+	gameObjectCards := map[int]int{}
+
+	for _, transaction := range transactions {
+		newPlayerSeats, playerSeatsOk := GetPlayerSeats(transaction)
+		newTurn, turnOk := GetTurn(transaction)
+		newHand, handOk := GetHands(transaction)
+		newGameObjectCards, gameObjectCardsOk := GetGameObjectCards(transaction)
+		winner, winnerOk := GetWinner(transaction)
+
+		if playerSeatsOk {
+			playerSeats = newPlayerSeats
+			for i, playerName := range playerSeats {
+				if playerName == userName {
+					userPlayerSeat = i
+				}
+			}
+			if userPlayerSeat == 0 {
+				log.Fatal("Could not find userName's player ID")
+			}
+		}
+		if turnOk {
+			turn = newTurn
+			_ = turn // Remove declared and not used during development
+		}
+		if handOk {
+			for _, k := range newHand[userPlayerSeat] {
+				hand[k] = struct{}{}
+			}
+		}
+		if gameObjectCardsOk {
+			for k, v := range newGameObjectCards {
+				gameObjectCards[k] = v
+			}
+		}
+		if winnerOk {
+			var wonOrLost string
+			if playerSeats[winner] == userName {
+				wonOrLost = "won"
+			} else {
+				wonOrLost = "lost"
+			}
+			handArray := []int{}
+			for k, _ := range hand {
+				handArray = append(handArray, k)
+			}
+			handCardNames := HandToCardNames(handArray, cardData, gameObjectCards)
+			return wonOrLost, handCardNames //+ GetGames(transactions[i+1:], cardData, userName)
+		}
+	}
+	return "", nil
+}
+
 func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	userName := "Jolteon#55824"
 
 	//mtgaLog, err := ioutil.ReadFile("C:\\Users\\joel\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\output_log.txt")
-	mtgaLog, err := ioutil.ReadFile("C:\\Users\\joel\\mtgaoutput\\output_log.txt")
+	mtgaLog, err := ioutil.ReadFile("C:\\Users\\joel\\mtgaoutput\\output_log2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	cardData := GetCardData()
 	transactions := GetTransactions(string(mtgaLog))
-	hands := GetHands(transactions)
-	gameObjectCards := GetGameObjectCards(transactions)
-	playerSeats := GetPlayerSeats(transactions)
-	winner := GetWinner(transactions)
-	
-	userNamePlayerSeat := 0
-	for i, playerName := range playerSeats {
-		if playerName == userName {
-			userNamePlayerSeat = i
-		}
-	}
-	if userNamePlayerSeat == 0 {
-		log.Fatal("Could not find userName's player ID")
-	}
 
-	handCardNames := handToCardNames(hands[userNamePlayerSeat], cardData, gameObjectCards)
-	printWinner(userName, playerSeats, winner, handCardNames)
+	fmt.Println(GetGames(transactions, cardData, userName))
 
 }
